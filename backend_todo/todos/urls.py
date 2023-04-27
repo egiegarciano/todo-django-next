db462a4d73386eeed0829c146157748e0a63ee31
Egie Garciano <egiegarciano14@gmail.com>
2023-04-27 14:04:24 +0800

    modified completedtodos api

--- backend_todo/todos/urls.py
+++ backend_todo/todos/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import CreateTodo, GetCurrentTodos, TodoDetail, GetCompletedTodos
+from .views import CreateTodo, GetCurrentTodos, TodoDetail, CompletedTodos
 
 urlpatterns = [
   path('current-todos/', GetCurrentTodos.as_view(), name='current-todos'),
-  path('completed-todos/', GetCompletedTodos.as_view(), name='completed-todos'),
+  path('completed-todos/', CompletedTodos.as_view(), name='completed-todos'),
   path('create-todo/', CreateTodo.as_view(), name='create-todo'),
   path('todo-detail/<int:pk>', TodoDetail.as_view(), name='todo-detail')
 ]
--- backend_todo/todos/views.py
+++ backend_todo/todos/views.py
@@ -4,6 +4,8 @@ from .serializers import TodoSerializer
 from django.utils import timezone
 from .models import Todo
 from .pagination import TodosPagination
+from rest_framework.response import Response
+from rest_framework import status
 
 class CreateTodo(generics.CreateAPIView):
   serializer_class = TodoSerializer
@@ -25,15 +27,25 @@ class GetCurrentTodos(generics.ListAPIView):
 
     return Todo.objects.filter(user=user, completed_at__isnull=True).order_by('-created_at')
 
-class GetCompletedTodos(generics.ListAPIView):
+class CompletedTodos(generics.GenericAPIView):
   serializer_class = TodoSerializer
   pagination_class = TodosPagination
 
-  def get_queryset(self):
-    auth_token = self.request.auth
+  def get(self, request, *args, **kwargs):
+    auth_token = request.auth
     user = Token.objects.get(key=auth_token).user
+    queryset = Todo.objects.filter(user=user, completed_at__isnull=False)
+    serializer = TodoSerializer(queryset, many=True)
 
-    return Todo.objects.filter(user=user, completed_at__isnull=False)
+    return self.get_paginated_response(self.paginate_queryset(serializer.data))
+
+  def delete(self, request, *args, **kwargs):
+    auth_token = request.auth
+    user = Token.objects.get(key=auth_token).user
+    queryset = Todo.objects.filter(user=user, completed_at__isnull=False)
+    queryset.delete()
+
+    return Response(status=status.HTTP_204_NO_CONTENT)
 
 class TodoDetail(generics.RetrieveUpdateDestroyAPIView):
   serializer_class = TodoSerializer
@@ -41,6 +53,8 @@ class TodoDetail(generics.RetrieveUpdateDestroyAPIView):
   def get_queryset(self):
     auth_token = self.request.auth
     user = Token.objects.get(key=auth_token).user
+
+    # Try to return error exceptions if id is not found
 
     return Todo.objects.filter(user=user, completed_at__isnull=True)
 
